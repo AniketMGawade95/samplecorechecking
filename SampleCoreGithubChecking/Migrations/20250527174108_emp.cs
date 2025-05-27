eaa05973ded42f5aa63440084a42394ccbf986e1
AniketMGawade95 <aniketmgawade2019@gmail.com>
2025-05-27 23:14:34 +0530

    fvsdgjf

---
+++ SampleCoreGithubChecking/Migrations/20250527174108_emp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleCoreGithubChecking.Migrations
+{
+    /// <inheritdoc />
+    public partial class emp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emps",
+                columns: table => new
+                {
+                    eid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emps", x => x.eid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SampleStudent",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    EnrollmentDate = table.Column<DateTime>(type: "date", nullable: true, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__SampleSt__32C52A79E79639AB", x => x.StudentID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emps");
+
+            migrationBuilder.DropTable(
+                name: "SampleStudent");
+        }
+    }
+}
---
+++ SampleCoreGithubChecking/Models/Emp.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleCoreGithubChecking.Models
+{
+    public class Emp
+    {
+        [Key]
+        public int eid { get; set; }
+
+        public string name { get; set; }
+    }
+}
